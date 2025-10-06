0cd63a17942dcf5b686a81ee70a9e6017ecd822a
iitmurcom <itimurweb@gmail.com>
2025-10-06 12:00:08 +0000

    ziphere vendor exclude

--- ziphere.sh
+++ ziphere.sh
@@ -22,6 +22,6 @@ archive_path="$(dirname "$PWD")/${archive_name}"
 
 # Create ZIP in the parent directory, excluding .git everywhere
 zip -r -9 "$archive_path" . \
-  -x "./.git" "./.git/*" "*/.git/*"
+  -x "./.git" "./.git/*" "*/.git/*" "./vendor/*" "./composer.*" "./phpunit.*" "./ziphere.sh"
 
 echo "Created: $archive_path"
