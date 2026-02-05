3fab4e2d27f2c37b2e3e806201abd7d684578c66
iitmurcom <itimurweb@gmail.com>
2026-02-05 10:00:57 +0000

    Update search input placeholder to indicate full search on Enter key

--- App/Views/pages/calendar.php
+++ App/Views/pages/calendar.php
@@ -33,7 +33,7 @@
     </header>
 
     <div class="filters">
-      <input id="filterText" class="input ev" type="search" placeholder="Пошук: назва або відповідальний">
+      <input id="filterText" class="input ev" type="search" placeholder="Пошук: назва або відповідальний (ENTER — повний пошук)">
       <button id="btnClearFilters" class="btn">Скинути</button>
     </div>
     <div id="quickFilters" class="quick-filters">
